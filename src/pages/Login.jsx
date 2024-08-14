f8a52b3c8b42ad696d0d5b7da39fcf81020c9476
younusFoysal <younus.faysal@gmail.com>
2024-08-15 00:59:51 +0600

    SignUp Page Done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import logo from '/logo.png'
 
 const Login = () => {
     return (
-        <div>
-            
+        <div
+            className=" min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-800 px-4 sm:px-6 lg:px-8">
+            <div className="relative py-3 sm:max-w-xs sm:mx-auto">
+                <div className="min-h-96 px-8 py-6 mt-4 text-left bg-white dark:bg-gray-900  rounded-xl shadow-lg">
+                    <div className="flex flex-col justify-center items-center h-full select-none">
+                        <div className="flex flex-col items-center justify-center gap-2 mb-8">
+                            <a href="/">
+                                <img src={logo} className="w-8"/>
+                            </a>
+                            <p className="m-0 text-[16px] font-semibold dark:text-white">Login to your Account</p>
+                            <span className="m-0 text-xs max-w-[90%] text-center text-[#8B8E98]">Get started with our app, just start section and enjoy experience.
+                    </span>
+                        </div>
+                        <div className="w-full flex flex-col gap-2">
+                            <label className="font-semibold text-xs text-gray-400 ">Email</label>
+                            <input
+                                className="border rounded-lg px-3 py-2 mb-5 text-sm w-full outline-none dark:border-gray-500 dark:bg-gray-900"
+                                placeholder="user@gmail.com"/>
+
+                        </div>
+                    </div>
+                    <div className="w-full flex flex-col gap-2">
+                        <label className="font-semibold text-xs text-gray-400 ">Password</label>
+                        <input type="password"
+                               className="border rounded-lg px-3 py-2 mb-5 text-sm w-full outline-none dark:border-gray-500 dark:bg-gray-900"
+                               placeholder="••••••••"/>
+
+                    </div>
+                    <div className="mt-5">
+                        <button
+                            className="py-1 px-8 bg-blue-500 hover:bg-blue-800 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg cursor-pointer select-none">Login
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
